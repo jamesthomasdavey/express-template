e96151cc8c5788c57f1472b3844a43a3edd0ac79
James Davey <james.t.davey@gmail.com>
2018-09-17 10:17:45 -0700

    add boilerplate code and personal styles for common express apps without react

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  const pageTitle = "Home";
+  res.render("home", { pageTitle });
+});
+
+app.get("*", (req, res) => {
+  res.send("Sorry, page not found.");
+});
+
+app.listen(3000, () => {
+  console.log("Starting...");
+  setTimeout(() => {
+    console.log("Your server has started, hue man.");
+  }, 1500);
+});
